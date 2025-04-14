152d6a633692329701af3e4b612ce16c526d22fa
Anton Maklakov <anton@espressif.com>
2025-04-14 15:27:41 +0700

    uart: fix esp32h2, esp32c6, and esp32 initialization for the early stages

--- src/esp32c6/src/uart.c
+++ src/esp32c6/src/uart.c
@@ -10,17 +10,19 @@
 
 // These functions are defined in the ROM
 extern void uartAttach(void *rxBuffer);
-extern void Uart_Init(uint8_t uart_no, uint32_t clock);
+extern void Uart_Init(uint8_t uart_no);
 extern uint32_t ets_clk_get_xtal_freq(void);
 extern void ets_update_cpu_frequency(uint32_t ticks_per_us);
-
-#define  APB_CLK_FREQ_ROM       (40 * 1000000)
+extern void ets_install_putc1(void (*p)(char c));
+extern void ets_install_putc2(void (*p)(char c));
 
 void stub_target_uart_init(uint8_t uart_num, uint32_t baudrate)
 {
     (void)baudrate;
     uartAttach(NULL);
-    uint32_t clock = ets_clk_get_xtal_freq();
-    ets_update_cpu_frequency(clock / 1000000);
-    Uart_Init(uart_num, APB_CLK_FREQ_ROM);
+    ets_update_cpu_frequency(ets_clk_get_xtal_freq() / 1000000);
+    Uart_Init(uart_num);
+
+    ets_install_putc1(NULL);
+    ets_install_putc2(NULL);
 }
