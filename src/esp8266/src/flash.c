cd2f0b301588f59ad212fb1a0f9dc1873abf3d3f
Anton Maklakov <anton@espressif.com>
2025-06-17 15:29:23 +0700

    feat(spiflash): add initial structure for SPI Flash support: ESP8266

--- src/esp8266/src/flash.c
+++ src/esp8266/src/flash.c
@@ -4,16 +4,24 @@
  * SPDX-License-Identifier: Apache-2.0 OR MIT
  */
 
+#include <log.h>
 #include <target/flash.h>
+#include <target/impl/flash_get_config_from_rom_old.h>
 
-void stub_target_flash_init(void *state)
+void stub_target_flash_init(void)
 {
-    (void)state;
+    STUB_LOG_TRACE();
     // TODO: Implement
 }
 
-void stub_target_flash_deinit(const void *state)
+uint32_t stub_target_flash_get_flash_id(void)
 {
-    (void)state;
+    STUB_LOG_TRACE();
     // TODO: Implement
+    return 0;
+}
+
+const esp_rom_spiflash_chip_t * stub_target_flash_get_config(void)
+{
+    return flash_impl_get_config_from_rom_old();
 }
