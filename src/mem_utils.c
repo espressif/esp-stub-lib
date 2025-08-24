408a6b6b75fcc35cdc7a485363a10b6989fc6241
Anton Maklakov <anton@espressif.com>
2025-08-25 01:36:01 +0700

    feat: add mem utils, for checking memory range types

---
+++ src/mem_utils.c
@@ -0,0 +1,73 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <mem_utils.h>
+
+#include <soc/soc_caps.h>
+#include <soc/soc.h>
+
+bool stub_lib_mem_is_irom(uintptr_t addr)
+{
+    return addr >= SOC_IROM_LOW && addr < SOC_IROM_HIGH;
+}
+
+bool stub_lib_mem_is_drom(uintptr_t addr)
+{
+    return addr >= SOC_DROM_LOW && addr < SOC_DROM_HIGH;
+}
+
+bool stub_lib_mem_is_iram(uintptr_t addr)
+{
+    return addr >= SOC_IRAM_LOW && addr < SOC_IRAM_HIGH;
+}
+
+bool stub_lib_mem_is_dram(uintptr_t addr)
+{
+    return addr >= SOC_DRAM_LOW && addr < SOC_DRAM_HIGH;
+}
+
+bool stub_lib_mem_is_rtc_iram_fast(uintptr_t addr)
+{
+#if defined(SOC_RTC_FAST_MEM_SUPPORTED) && SOC_RTC_FAST_MEM_SUPPORTED
+    return addr >= SOC_RTC_IRAM_LOW && addr < SOC_RTC_IRAM_HIGH;
+#else
+    (void)addr;
+    return false;
+#endif
+}
+
+bool stub_lib_mem_is_rtc_dram_fast(uintptr_t addr)
+{
+#if defined(SOC_RTC_FAST_MEM_SUPPORTED) && SOC_RTC_FAST_MEM_SUPPORTED
+    return addr >= SOC_RTC_DRAM_LOW && addr < SOC_RTC_DRAM_HIGH;
+#else
+    (void)addr;
+    return false;
+#endif
+
+}
+
+bool stub_lib_mem_is_rtc_slow(uintptr_t addr)
+{
+#if defined(SOC_RTC_SLOW_MEM_SUPPORTED) && SOC_RTC_SLOW_MEM_SUPPORTED
+    return addr >= SOC_RTC_DATA_LOW && addr < SOC_RTC_DATA_HIGH;
+#else
+    (void)addr;
+    return false;
+#endif
+}
+
+bool stub_lib_mem_is_tcm(uintptr_t addr)
+{
+#if defined(SOC_MEM_TCM_SUPPORTED) && SOC_MEM_TCM_SUPPORTED
+    return (addr >= SOC_TCM_LOW && addr < SOC_TCM_HIGH);
+#else
+    (void)addr;
+    return false;
+#endif
+}
