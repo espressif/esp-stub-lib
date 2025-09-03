354dfb005156c2220de79f60f43a277280b1e8e4
Radim Karniš <radim.karnis@espressif.com>
2025-09-03 13:59:58 +0200

    feat: Add wrappers for UART RX/TX ROM funcs

--- include/esp-stub-lib/rom_wrappers.h
+++ include/esp-stub-lib/rom_wrappers.h
@@ -8,5 +8,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 void esp_stub_lib_delay_us(uint32_t us);
 uint16_t esp_stub_lib_crc16_le(uint16_t crc, const uint8_t *buf, uint32_t len);
+void esp_stub_lib_tx_one_char(uint8_t c);
+uint8_t esp_stub_lib_rx_one_char(void);
+void esp_stub_lib_tx_flush(void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
