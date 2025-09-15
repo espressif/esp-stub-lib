03cc7dd951ee86d24e0f9d02e48e367302ab0628
Anton Maklakov <anton@espressif.com>
2025-09-15 10:18:59 +0700

    fixup

--- include/esp-stub-lib/mem_utils.h
+++ include/esp-stub-lib/mem_utils.h
@@ -7,37 +7,38 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * @brief Check if address is in external memory range.
  *
  * External flash memory mapped via the instruction bus.
  */
-bool stub_lib_mem_is_irom(const void *p);
+bool stub_lib_mem_is_irom(uintptr_t addr);
 
 /**
  * @brief Check if address is in external memory range.
  *
  * External flash memory mapped via the data bus.
  */
-bool stub_lib_mem_is_drom(const void *p);
+bool stub_lib_mem_is_drom(uintptr_t addr);
 
 /**
  * @brief Check if address is in internal memory range.
  *
  * Internal memory accessed via the instruction bus.
  */
-bool stub_lib_mem_is_iram(const void *p);
+bool stub_lib_mem_is_iram(uintptr_t addr);
 
 /**
  * @brief Check if address is in internal memory range.
  *
  * Internal memory accessed via the data bus.
  */
-bool stub_lib_mem_is_dram(const void *p);
+bool stub_lib_mem_is_dram(uintptr_t addr);
 
-bool stub_lib_mem_is_rtc_iram_fast(const void *p);
-bool stub_lib_mem_is_rtc_dram_fast(const void *p);
-bool stub_lib_mem_is_rtc_slow(const void *p);
+bool stub_lib_mem_is_rtc_iram_fast(uintptr_t addr);
+bool stub_lib_mem_is_rtc_dram_fast(uintptr_t addr);
+bool stub_lib_mem_is_rtc_slow(uintptr_t addr);
 
-bool stub_lib_mem_is_tcm(const void *p);
+bool stub_lib_mem_is_tcm(uintptr_t addr);
--- src/mem_utils.c
+++ src/mem_utils.c
@@ -10,79 +10,83 @@
 
 #include <soc/soc.h>
 
-bool stub_lib_mem_is_irom(const void *p)
+bool stub_lib_mem_is_irom(uintptr_t addr)
 {
 #if defined(SOC_IROM_LOW) && defined(SOC_IROM_HIGH)
-    return (uintptr_t)p >= SOC_IROM_LOW && (uintptr_t)p < SOC_IROM_HIGH;
+    return addr >= SOC_IROM_LOW && addr < SOC_IROM_HIGH;
 #else
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_drom(const void *p)
+bool stub_lib_mem_is_drom(uintptr_t addr)
 {
 #if defined(SOC_DROM_LOW) && defined(SOC_DROM_HIGH)
-    return (uintptr_t)p >= SOC_DROM_LOW && (uintptr_t)p < SOC_DROM_HIGH;
+    return addr >= SOC_DROM_LOW && addr < SOC_DROM_HIGH;
 #else
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_iram(const void *p)
+bool stub_lib_mem_is_iram(uintptr_t addr)
 {
 #if defined(SOC_IRAM_LOW) && defined(SOC_IRAM_HIGH)
-    return (uintptr_t)p >= SOC_IRAM_LOW && (uintptr_t)p < SOC_IRAM_HIGH;
+    return addr >= SOC_IRAM_LOW && addr < SOC_IRAM_HIGH;
 #else
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_dram(const void *p)
+bool stub_lib_mem_is_dram(uintptr_t addr)
 {
 #if defined(SOC_DRAM_LOW) && defined(SOC_DRAM_HIGH)
-    return (uintptr_t)p >= SOC_DRAM_LOW && (uintptr_t)p < SOC_DRAM_HIGH;
+    return addr >= SOC_DRAM_LOW && addr < SOC_DRAM_HIGH;
 #else
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_rtc_iram_fast(const void *p)
+bool stub_lib_mem_is_rtc_iram_fast(uintptr_t addr)
 {
 #if defined(SOC_RTC_IRAM_LOW) && defined(SOC_RTC_IRAM_HIGH)
-    return (uintptr_t)p >= SOC_RTC_IRAM_LOW && (uintptr_t)p < SOC_RTC_IRAM_HIGH;
+    return addr >= SOC_RTC_IRAM_LOW && addr < SOC_RTC_IRAM_HIGH;
 #else
-    (void)p;
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_rtc_dram_fast(const void *p)
+bool stub_lib_mem_is_rtc_dram_fast(uintptr_t addr)
 {
 #if defined(SOC_RTC_DRAM_LOW) && defined(SOC_RTC_DRAM_HIGH)
-    return (uintptr_t)p >= SOC_RTC_DRAM_LOW && (uintptr_t)p < SOC_RTC_DRAM_HIGH;
+    return addr >= SOC_RTC_DRAM_LOW && addr < SOC_RTC_DRAM_HIGH;
 #else
-    (void)p;
+    (void)addr;
     return false;
 #endif
 
 }
 
-bool stub_lib_mem_is_rtc_slow(const void *p)
+bool stub_lib_mem_is_rtc_slow(uintptr_t addr)
 {
 #if defined(SOC_RTC_DATA_LOW) && defined(SOC_RTC_DATA_HIGH)
-    return (uintptr_t)p >= SOC_RTC_DATA_LOW && (uintptr_t)p < SOC_RTC_DATA_HIGH;
+    return addr >= SOC_RTC_DATA_LOW && addr < SOC_RTC_DATA_HIGH;
 #else
-    (void)p;
+    (void)addr;
     return false;
 #endif
 }
 
-bool stub_lib_mem_is_tcm(const void *p)
+bool stub_lib_mem_is_tcm(uintptr_t addr)
 {
 #if defined(SOC_TCM_LOW) && defined(SOC_TCM_HIGH)
-    return ((uintptr_t)p >= SOC_TCM_LOW && (uintptr_t)p < SOC_TCM_HIGH);
+    return (addr >= SOC_TCM_LOW && addr < SOC_TCM_HIGH);
 #else
-    (void)p;
+    (void)addr;
     return false;
 #endif
 }
