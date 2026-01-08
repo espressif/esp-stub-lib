545d100ba88227b269eb79941d7b2ade2a106d0e
Radim Karniš <radim.karnis@espressif.com>
2026-01-08 16:02:34 +0100

    fix(clock): Wait for the clocks to stabilize after init

--- src/clock.c
+++ src/clock.c
@@ -1,14 +1,16 @@
 /*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2025-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0 OR MIT
  */
 
 #include <target/clock.h>
+#include <rom_wrappers.h>
 
 void stub_lib_clock_init(void)
 {
     stub_target_clock_init();
+    stub_lib_delay_us(50); // Wait for the clocks to stabilize
 }
 
 void stub_lib_clock_disable_watchdogs(void)
