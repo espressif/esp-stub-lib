148981510b83666b834f51100d5205a61510cfea
Vojtech Piroch <vojtech.piroch@espressif.com>
2026-04-30 10:04:28 +0200

    feat: Add configurable output for logging

--- src/log_uart.c
+++ src/log_uart.c
@@ -11,11 +11,22 @@
 extern void ets_install_putc1(void (*p)(char c));
 extern void ets_install_putc2(void (*p)(char c));
 extern void ets_install_uart_printf(void);
+extern void ets_install_usb_printf(void);
+
+#if !defined(STUB_LIB_LOG_UART_NUM)
+#define STUB_LIB_LOG_UART_NUM 0
+#endif
 
 void stub_lib_log_backend_init(void)
 {
-    stub_target_uart_init(0);
+#if defined(STUB_LIB_LOG_TO_USB_SERIAL_JTAG) && (STUB_LIB_LOG_TO_USB_SERIAL_JTAG)
+    ets_install_putc1(NULL);
+    ets_install_putc2(NULL);
+    ets_install_usb_printf();
+#else
+    stub_target_uart_init((uint8_t)STUB_LIB_LOG_UART_NUM);
     ets_install_putc1(NULL);
     ets_install_putc2(NULL);
     ets_install_uart_printf();
+#endif
 }
